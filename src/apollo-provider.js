ae4df4309d94859adace1b3b73b2ccda4bbc689f
Jake Ho <jake.ho@scmp.com>
2018-03-16 16:21:26 +0800

    attempt to fix the error: TS2339: Property 'defaultClient' does not exist on type 'Subscribe<any> | Query<any> | Mutate<any, any>'.   Property 'defaultClient' does not exist on type 'Subscribe<any>'.

--- src/apollo-provider.js
+++ src/apollo-provider.js
@@ -3,6 +3,14 @@ import { VUE_APOLLO_QUERY_KEYWORDS } from './consts'
 import { getMergedDefinition } from './utils'
 
 export class ApolloProvider {
+
+  clients
+  defaultClient
+  defaultOptions
+  watchLoading
+  errorHandler
+  prefetchQueries = []
+
   constructor (options) {
     if (!options) {
       throw new Error('Options argument required')
